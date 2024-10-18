4e72e349c8b092985763fc13967bacf94eb17be5
Simone-Domenici <domenici.simone00@gmail.com>
2024-10-18 14:56:22 +0200

    ADD sum between user number and cpu number

--- js/evenodd.js
+++ js/evenodd.js
@@ -1,5 +1,22 @@
 const evenOrOdd = prompt('Scegli pari o dispari')
 const userNumber = parseInt(prompt('Scegli un numero da 1 a 5'))
 
-const compNumber = parseInt(Math.random() * 5 + 1)
-console.log(evenOrOdd, userNumber, compNumber)
+function cpuNumberOneToFive() {
+    const compNumber = parseInt(Math.random() * 5 + 1)
+    // console.log(evenOrOdd, userNumber, compNumber)
+    return compNumber
+}
+
+const cpuNumber = cpuNumberOneToFive()
+
+function sum(num1,num2) {
+    let result = num1 + num2
+
+    return result
+}
+
+sum(userNumber,cpuNumber)
+
+console.log(userNumber, cpuNumber)
+
+console.log(sum(userNumber,cpuNumber))
