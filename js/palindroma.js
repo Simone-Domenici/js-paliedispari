71fe4d11370697ad1fccf631c26733faf0515da3
Simone-Domenici <domenici.simone00@gmail.com>
2024-10-18 14:36:15 +0200

    ADD palindrome exercise

--- js/palindroma.js
+++ js/palindroma.js
@@ -1,1 +1,26 @@
-let word = prompt('Inserisci Parola')
+const word = prompt('Inserisci Parola')
+
+function isPalindrome(text) {
+
+    let reversedWord = ''
+
+    for (let i = 0; i < text.length ; i++) {
+        const char = text.at(-1 -i)  
+        reversedWord += char.toLowerCase()
+    }
+    // console.log(reversedWord)
+
+    return reversedWord
+    
+}
+
+isPalindrome(word)
+
+// console.log(isPalindrome(word))
+// console.log(word.toLowerCase())
+
+if (isPalindrome(word) === word.toLowerCase()) {
+    console.log('Sono palindrome')
+} else {
+    console.log('Non sono palindrome')
+}
